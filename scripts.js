565460015e0a1a315fc35e9fefd6621b102f66d6
bgriessbach <b.griessbach@gmail.com>
2017-07-18 22:42:00 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,53 +1,53 @@
-var items=[["52.075,-0.7333", "Walthamstow", "Hour_Home"], ["51.5188,0.0814","Work", "Hour_Work"], ["51.612789,-0.017634", "Chingford", "Hour_Charity"]];
-var APIKEY=[KEY];
+var location_data=[["52.075,-0.7333", "Home", "Hour_Home"], ["51.5188,0.0814","Work", "Hour_Work"], ["51.612789,-0.017634", "Charity", "Hour_Charity"]];
+var APIKEY="5b4fe673c6e4ab2092645198014f0dff";
 
-for (var i=0; i<items.length; i++){
-    weather(items[i][0], APIKEY, items[i][1], items[i][2]);
+for (var i=0; i<location_data.length; i++){
+    createWeather(location_data[i][0], APIKEY, location_data[i][1], location_data[i][2]);
 }
 
 
-function weather (geo, APIKEY, ID, H_ID) {
+function createWeather (geo_location, APIKEY, p_ID, table_ID) {
     $.getJSON(
-    "https://api.darksky.net/forecast/"+APIKEY+"/"+geo+"?exclude=minutely,daily,alerts,flags&units=si",
+    "https://api.darksky.net/forecast/"+APIKEY+"/"+geo_location+"?exclude=minutely,daily,alerts,flags&units=si",
     {},
-    function(object) {
-        var hour="<b>Forecast: </b>"+JSON.stringify(object.hourly.summary)+"<br>";
-        var h_temp=[]
-        var h_time=[]
-        for (var y=0; y<object.hourly.data.length; y+=2){
-            h_temp.push(JSON.stringify(object.hourly.data[y].temperature)+" \xB0"+"C");
-            var convert=timeconvert(JSON.stringify(object.hourly.data[y].time));
-            h_time.push(convert);
-            if (h_time.length>7){
+    function(weather_data) {
+        createWeatherTable(weather_data, table_ID);
+        createCurrentWeather(weather_data, p_ID);
+    }
+    );
+}
+
+function createCurrentWeather(weather_data, p_ID){
+    var hourly_summary="<b>Forecast: </b>"+JSON.stringify(weather_data.hourly.summary)+"<br>";       
+    var timestamp=timeconvert(JSON.stringify(weather_data.currently.time));
+    document.getElementById(p_ID).innerHTML =
+    '<b>Time: </b>' +timestamp+ "<br>" +
+    '<b>Summary: </b>' + JSON.stringify(weather_data.currently.summary)+"<br>" +
+    '<b>Temperature: </b>'+ JSON.stringify(weather_data.currently.temperature)+' \xB0'+'C'+"<br>"+hourly_summary;
+}
+
+function createWeatherTable(weather_data, table_ID){
+        var weather_temperature="";
+        var weather_time="";
+        for (var y=0; y<weather_data.hourly.data.length; y+=2){
+            weather_temperature+="<td>"+JSON.stringify(weather_data.hourly.data[y].temperature)+" \xB0"+"C"+"</td>";
+            var convert=timeconvert(JSON.stringify(weather_data.hourly.data[y].time));
+            weather_time+="<td>"+convert+"</td>";
+            if (y>10){
                 break;
             }
             
         }
         
-        var table=document.getElementById(H_ID);
-        var row0= table.insertRow(0)
-        var row1= table.insertRow(1)
-        for (var z=0; z<h_temp.length; z++){
-            var cell_t=row0.insertCell(z);
-            var cell_te=row1.insertCell(z);
-            cell_t.innerHTML=h_time[z];
-            cell_te.innerHTML=h_temp[z];
-        }
+        document.getElementById(table_ID).innerHTML=
+        "<tr><td><b>Time</b></td>"+weather_time+
+        "<tr><td><b>Temperature</b></td>"+weather_temperature;
 
-        var timestamp=timeconvert(JSON.stringify(object.currently.time));
-        document.getElementById(ID).innerHTML =
-'<b>Time: </b>' +timestamp+ "<br>" +
-'<b>Summary: </b>' + JSON.stringify(object.currently.summary)+"<br>" +
-'<b>Temperature: </b>'+ JSON.stringify(object.currently.temperature)+' \xB0'+'C'+"<br>"+hour;
-}
-
-);
 }
 
 function timeconvert(unix_timestamp){
-var date = new Date(unix_timestamp*1000);
-var hours = ("0"+ date.getHours()).substr(-2);
-var minutes = ("0" + date.getMinutes()).substr(-2);
-var seconds = ("0" + date.getSeconds()).substr(-2);
-return hours + ':' + minutes+ ':' + seconds
+    var date = new Date(unix_timestamp*1000);
+    var hours = ("0"+ date.getHours()).substr(-2);
+    var minutes = ("0" + date.getMinutes()).substr(-2);
+    return hours + ':' + minutes
 }
